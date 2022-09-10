dd067007909389998ec36d56c6aa51341c952a3f
centsent <wanzau@proton.me>
2022-09-10 10:42:09 +0800

    refactor: update java lsp config

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -3,12 +3,14 @@ if not has_jdtls then
   return
 end
 
+local jdtls_setup = require("jdtls.setup")
+local mylsp = require("me.lsp")
 local home = os.getenv("HOME")
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
-local workspace_dir = home .. "/workspace/java/" .. project_name
+local workspace_dir = home .. "/.cache/jdtls-workspace" .. project_name
 local jdtls_install_dir = home .. "/.local/share/nvim/mason/packages/jdtls"
-local launcher_version = "1.6.400.v20210924-0641"
-local root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew" })
+local launcher_version = "1.6.400.v2021024-0641"
+local root_dir = jdtls_setup.find_root({ ".git", "mvnw", "gradlew" })
 local extendedClientCapabilities = jdtls.extendedClientCapabilities
 extendedClientCapabilities.resolveAdditionalTextEditsSupport = true
 
@@ -28,11 +30,68 @@ local cmd = {
 
   "-jar",
   jdtls_install_dir .. "/plugins/org.eclipse.equinox.launcher_" .. launcher_version .. ".jar",
-
   "-configuration",
   jdtls_install_dir .. "/config_linux",
   "-data",
   workspace_dir,
+}
+
+-- Here you can configure eclipse.jdt.ls specific settings
+-- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+-- for a list of options
+local java_configuration = {
+  eclipse = {
+    downloadSources = true,
+  },
+  maven = {
+    downloadSources = true,
+  },
+  implementationsCodeLens = {
+    enabled = true,
+  },
+  referencesCodeLens = {
+    enabled = true,
+  },
+  references = {
+    includeDecompiledSources = true,
+  },
+  inlayHints = {
+    parameterNames = {
+      enabled = "all", -- literals, all, none
+    },
+  },
+  format = {
+    enabled = false,
+  },
+  completion = {
+    favoriteStaticMembers = {
+      "org.assertj.core.api.Assertions.assertThat",
+      "org.assertj.core.api.Assertions.assertThatThrownBy",
+      "org.assertj.core.api.Assertions.assertThatExceptionOfType",
+      "org.assertj.core.api.Assertions.catchThrowable",
+      "org.hamcrest.MatcherAssert.assertThat",
+      "org.hamcrest.Matchers.*",
+      "org.hamcrest.CoreMatchers.*",
+      "org.junit.jupiter.api.Assertions.*",
+      "java.util.Objects.requireNonNull",
+      "java.util.Objects.requireNonNullElse",
+      "org.mockito.Mockito.*",
+    },
+  },
+  signatureHelp = { enabled = true },
+  contentProvider = { preferred = "fernflower" },
+  sources = {
+    organizeImports = {
+      starThreshold = 999,
+      staticStarThreshold = 9999,
+    },
+  },
+  codeGeneration = {
+    toString = {
+      template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+    },
+    useBlocks = true,
+  },
 }
 
 -- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
@@ -40,99 +99,10 @@ local config = {
   -- The command that starts the language server
   -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
   cmd = cmd,
-
   root_dir = root_dir,
-  filetypes = { "java" },
-
-  -- Here you can configure eclipse.jdt.ls specific settings
-  -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
-  -- for a list of options
   settings = {
-    java = {
-      signatureHelp = { enabled = true },
-      contentProvider = { preferred = "fernflower" },
-      import = {
-        exclusions = {
-          "**/node_modules/**",
-          "**/.metadata/**",
-          "**/archetype-resources/**",
-          "**/META-INF/maven/**",
-          "**/.git/**",
-          "**/.idea/**",
-        },
-      },
-      templates = {
-        fileHeader = {
-          "/**",
-          " * ${type_name}",
-          " * @author ${user}",
-          " */",
-        },
-        typeComment = {
-          "/**",
-          " * ${type_name}",
-          " * @author ${user}",
-          " */",
-        },
-      },
-      eclipse = {
-        downloadSources = true,
-      },
-      maven = {
-        downloadSources = true,
-        updateSnapshots = true,
-      },
-      server = {
-        launchMode = "Hybrid",
-      },
-      completion = {
-        favoriteStaticMembers = {
-          "org.assertj.core.api.Assertions.assertThat",
-          "org.assertj.core.api.Assertions.assertThatThrownBy",
-          "org.assertj.core.api.Assertions.assertThatExceptionOfType",
-          "org.assertj.core.api.Assertions.catchThrowable",
-          "org.hamcrest.MatcherAssert.assertThat",
-          "org.hamcrest.Matchers.*",
-          "org.hamcrest.CoreMatchers.*",
-          "org.junit.jupiter.api.Assertions.*",
-          "java.util.Objects.requireNonNull",
-          "java.util.Objects.requireNonNullElse",
-          "org.mockito.Mockito.*",
-        },
-        filteredTypes = {
-          "com.sun.*",
-          "io.micrometer.shaded.*",
-          "java.awt.*",
-          "jdk.*",
-          "sun.*",
-        },
-      },
-      sources = {
-        organizeImports = {
-          starThreshold = 9999,
-          staticStarThreshold = 9999,
-        },
-      },
-      codeGeneration = {
-        toString = {
-          template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
-        },
-        hashCodeEquals = {
-          useJava7Objects = true,
-        },
-        useBlocks = true,
-      },
-      --configuration = {
-      --runtimes = {
-      --{
-      --name = "JavaSE-18",
-      --path = "/usr/local/opt/openjdk/libexec/openjdk.jdk/Contents/Home/",
-      --},
-      --},
-      --},
-    },
+    java = java_configuration,
   },
-
   -- Language server `initializationOptions`
   -- You need to extend the `bundles` with paths to jar files
   -- if you want to use additional eclipse.jdt.ls plugins.
@@ -144,7 +114,6 @@ local config = {
     --bundles = {},
     extendedClientCapabilities = extendedClientCapabilities,
   },
-
   autostart = true,
   flags = {
     allow_incremental_sync = true,
@@ -154,10 +123,9 @@ local config = {
 }
 
 config.on_attach = function(client, bufnr)
-  require("me.lsp").on_attach(client, bufnr)
-  require("jdtls.setup").add_commands()
+  mylsp.on_attach(client, bufnr)
+  jdtls_setup.add_commands()
 end
 
-config.capabilities = require("me.lsp").make_capabilities()
-
+config.capabilities = mylsp.make_capabilities()
 jdtls.start_or_attach(config)
