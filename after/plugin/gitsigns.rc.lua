f2597dcdd5efa92b73d2ef670cfd023d71743421
centsent <wanzau@proton.me>
2022-09-14 17:04:35 +0800

    refactor: update gitsigns config

--- after/plugin/gitsigns.rc.lua
+++ after/plugin/gitsigns.rc.lua
@@ -1,6 +1,16 @@
-local ok, gitsigns = pcall(require, "gitsigns")
-if (not ok) then return end
+local has_gitsigns, gitsigns = pcall(require, "gitsigns")
+if not has_gitsigns then
+  return
+end
 
-gitsigns.setup({
+local settings = {
   current_line_blame = true,
-})
+  numhl = true,
+  word_diff = true,
+}
+
+local setup = function(user_config)
+  gitsigns.setup(user_config)
+end
+
+setup(settings)
