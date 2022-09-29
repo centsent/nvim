2e859cda88ad59d4614d0d5c6ebc1369b69410ea
centsent <wanzau@proton.me>
2022-09-29 22:13:46 +0800

    refactor: update notify settings

--- after/plugin/nvim-notify.rc.lua
+++ after/plugin/nvim-notify.rc.lua
@@ -3,4 +3,15 @@ if not has_notify then
   return
 end
 
-vim.notify = notify
+local settings = {
+  background_colour = "#121212",
+  max_width = 80,
+}
+
+local setup = function(user_opts)
+  notify.setup(user_opts)
+  vim.notify = notify
+  vim.api.nvim_set_keymap("n", "<leader>n", "", { callback = notify.dismiss })
+end
+
+setup(settings)
