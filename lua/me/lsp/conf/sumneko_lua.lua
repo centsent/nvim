e3d1985839314269a03ee450960500c35efbe4e0
centsent <wanzau@proton.me>
2022-10-15 17:28:14 +0800

    refactor: update neodev config

--- lua/me/lsp/conf/sumneko_lua.lua
+++ lua/me/lsp/conf/sumneko_lua.lua
@@ -1,25 +1,20 @@
 local safe_require = require("utils").safe_require
-
-local make_config = function()
-  local has_neodev, neodev = safe_require("neodev")
-  if not has_neodev then
-    return {}
-  end
-
-  local config = neodev.setup({
-    runtime_path = true,
-    library = {
-      vimruntime = true,
-      types = true,
-      plugins = true,
-    },
-    lspconfig = {
-      runtime = { version = "LuaJIT" },
-      global = { "vim" },
-    },
-  })
-
-  return config
+local has_lspconfig, lspconfig = safe_require("lspconfig")
+if not has_lspconfig then
+  return
 end
 
-require("me.lsp").setup_with_config("sumneko_lua", make_config())
+local has_neodev, neodev = safe_require("neodev")
+if has_neodev then
+  neodev.setup()
+end
+
+lspconfig.sumneko_lua.setup({
+  settings = {
+    Lua = {
+      completion = {
+        callSnippet = "Replace",
+      },
+    },
+  },
+})
